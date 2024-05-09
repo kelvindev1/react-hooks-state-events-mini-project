1e34d96381fe976d7d9d471cbcc0b58f23fb085a
kelvindev1 <kelvinmutugi336@gmail.com>
2024-05-09 22:26:33 +0300

    solved lab

--- src/__tests__/Task.test.js
+++ src/__tests__/Task.test.js
@@ -5,20 +5,46 @@ import Task from "../components/Task";
 
 test("displays the task text", () => {
   render(<Task text={"text!"} category={"category!"} />);
-  expect(screen.queryByText("text!")).toBeInTheDocument();
+  const taskText = screen.getByText((content, element) => {
+    if (
+      element.tagName.toLowerCase() === "div" &&
+      element.classList.contains("text")
+    ) {
+      return content.trim() === "text!";
+    }
+    return false;
+  });
+  expect(taskText).toBeInTheDocument();
 });
 
 test("displays the task category", () => {
   render(<Task text={"text!"} category={"category!"} />);
-  expect(screen.queryByText("category!")).toBeInTheDocument();
+  const taskCategory = screen.getByText((content, element) => {
+    if (
+      element.tagName.toLowerCase() === "div" &&
+      element.classList.contains("label")
+    ) {
+      return content.trim() === "category!";
+    }
+    return false;
+  });
+  expect(taskCategory).toBeInTheDocument();
 });
 
 test("is removed from the list when the delete button is clicked", () => {
   render(<App />);
-  const task = screen.queryByText(/Buy rice/);
+  const task = screen.getByText((content, element) => {
+    if (
+      element.tagName.toLowerCase() === "div" &&
+      element.classList.contains("text")
+    ) {
+      return content.trim() === "Buy rice";
+    }
+    return false;
+  });
   const deleteButton = task.parentElement.querySelector("button");
 
   fireEvent.click(deleteButton);
 
-  expect(screen.queryByText(/Buy rice/)).not.toBeInTheDocument();
+  expect(screen.queryByText("Buy rice")).toBeNull();
 });
